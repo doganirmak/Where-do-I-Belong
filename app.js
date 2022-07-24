d58b667615fc180342b59f25126c28337af2bf21
dogan.irmak <dogan.irmak@icloud.com>
2022-07-24 17:51:53 +0300

    mutation commit

--- app.js
+++ app.js
@@ -15,3 +15,16 @@ function compare(a, b){
   }
   
   getIndexToIns([40, 60], 50);
+
+  function mutation(arr) {
+    let arr1 = arr[0].toLowerCase();
+    let arr2 = arr[1].toLowerCase();
+    for (let i = 0; i < arr2.length; i++) {
+      if (arr1.indexOf(arr2[i]) < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  mutation(["hello", "hey"]);
